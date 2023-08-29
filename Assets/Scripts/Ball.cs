56d82cf6a0d55064c051e0f6bdb9fcb4dbe24037
Andrey Moskalenko <eosproject.director@gmail.com>
2023-08-29 12:28:28 +0300

    Finished anti-loop mechanics

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,10 +5,16 @@ namespace Arkanoid
 {
     public class Ball : MonoBehaviour
     {
+        [SerializeField] private float deltaWallYForImpulse = 0.1f;
+        [SerializeField] private float antiCircleImpulse = 1f;
         [Inject] private EventsManager eventsManager;
+
+        private Rigidbody2D rb;
+        private float lastWallY;
 
         private void Start()
         {
+            rb = gameObject.GetComponent<Rigidbody2D>();
             eventsManager.AddListener(GameEvents.WinGame, _ => gameObject.SetActive(false));
             eventsManager.AddListener(GameEvents.LoseGame, _ => gameObject.SetActive(false));
         }
@@ -19,6 +25,28 @@ namespace Arkanoid
             {
                 Destroy(collision.gameObject);
                 eventsManager.InvokeEvent(GameEvents.DestroyObstacle);
+            }
+
+            if (collision.gameObject.CompareTag("Border"))
+            {
+                if (Mathf.Abs(transform.position.y - lastWallY) <= deltaWallYForImpulse)    // Избавляемся от зацикливания, когда шарик прыгает влево-вправо от одной стены к другой
+                {
+                    float speed = rb.velocity.magnitude;  // Запоминаем скорость, чтобы при добавлении импульса общая скорость не увеличилась, только направление
+
+                    Vector2 direction = Random.Range(0f, 1f) >= 0.5f ? Vector2.up : Vector2.down;
+                    rb.AddForce(direction * antiCircleImpulse, ForceMode2D.Impulse);
+
+                    rb.velocity = rb.velocity.normalized * speed;
+                    lastWallY = 0f;
+                }
+                else
+                {
+                    lastWallY = transform.position.y;
+                }
+            }
+            else
+            {
+                lastWallY = 0f;
             }
 
             if (collision.gameObject.CompareTag("Death"))
--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -4,17 +4,27 @@ namespace Arkanoid
 {
     public class PlatformController : MonoBehaviour
     {
+        [Header("Input Settings")]
         [SerializeField] private InputController inputController;
-
         [SerializeField] private float movementSentence = 0.025f;
         [SerializeField] private float movementLimit = 1.68f;
-        [SerializeField] private float force = 10f;
-        [SerializeField] private Rigidbody2D ball;
 
+        [Header("Bounce Settings")]
+        [SerializeField] private Rigidbody2D ball;
+        [SerializeField] private float force = 10f;
+        [SerializeField] private float sideImpulse = 1f;
+
+        [SerializeField][Range(0f, 1f)] private float normalSideZone = 0.5f;   // Зона на платформе от 0 до 1, при попадании на которую шарик будет отскакивать обычным образом.
+                                                                               // За пределами этой зоны (шарик приземлился ближе к левой или правой части платформы)
+                                                                               // будет добавлен импульс в сторону (sideImpulse). Нужно, чтобы легче управлять шариком,
+                                                                               // чтобы он не прыгал только вперед - назад от платформы к препятствиям и обратно.
+        private Vector2 platformSize;
         private bool isBallOnPlatform;
                         
         private void Start()
         {
+            platformSize = gameObject.GetComponent<SpriteRenderer>().size;
+
             ball.transform.SetParent(transform);
             isBallOnPlatform = true;
 
@@ -38,6 +48,27 @@ namespace Arkanoid
                 ball.AddForce(Vector2.up * force, ForceMode2D.Impulse);
 
                 isBallOnPlatform = false;
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            Ball target = collision.gameObject.GetComponent<Ball>();
+
+            if (target == null)
+                return;
+
+            float deltaPosition = Mathf.Abs(target.transform.position.x - transform.position.x);
+            float minZone = platformSize.x * normalSideZone;
+
+            if (deltaPosition > minZone)
+            {
+                float speed = ball.velocity.magnitude;
+                
+                float direction = target.transform.position.x >= transform.position.x ? 1f : -1f;                
+                ball.AddForce(Vector2.right * sideImpulse * direction, ForceMode2D.Impulse);
+                
+                ball.velocity = ball.velocity.normalized * speed;
             }
         }
     }
